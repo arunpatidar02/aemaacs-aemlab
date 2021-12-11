c6ed0276f3f3df9113459293dbc0e73f7a71de79
Arun Patidar <arunpatidar02>
2021-12-11 17:06:56 +0100

    test servlet

---
+++ core/src/main/java/com/community/aemlab/core/servlets/ProductSearchServlet.java
@@ -0,0 +1,38 @@
+package com.community.aemlab.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.propertytypes.ServiceDescription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(service = { Servlet.class }, immediate = true)
+@SlingServletResourceTypes(resourceTypes = "aemlab/utils/product/search", methods = HttpConstants.METHOD_GET, selectors = "product-search", extensions = "html")
+@ServiceDescription("Product Search Servlet")
+public class ProductSearchServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductSearchServlet.class);
+
+    @Activate
+    public void activate() {
+        LOGGER.trace("ProductSearchServlet.class Activated");
+    }
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        LOGGER.trace("Start");
+        response.getWriter().write("Hello");
+        LOGGER.trace("Ends");
+    }
+
+}
