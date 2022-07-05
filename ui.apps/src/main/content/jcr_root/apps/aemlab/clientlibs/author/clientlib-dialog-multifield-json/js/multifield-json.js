f63cfcb5d0f314f17dac2af0c9a6821f86105787
Arun Patidar <arunpatidar02>
2022-07-05 15:28:26 +0200

    json function

--- ui.apps/src/main/content/jcr_root/apps/aemlab/clientlibs/author/clientlib-dialog-multifield-json/js/multifield-json.js
+++ ui.apps/src/main/content/jcr_root/apps/aemlab/clientlibs/author/clientlib-dialog-multifield-json/js/multifield-json.js
@@ -1,53 +1,80 @@
-console.log("client multifields....");
+
 (function (document, $, ns) {
     "use strict";
+
     $(document).on("click", ".cq-dialog-submit", function (e) {
-
-
         $('.multifield-save-is-json').each(function () {
             var $multifield = $(this);
-                   var multifieldname = $multifield.attr('data-granite-coral-multifield-name');
-            
-                 //console.log($multifield.find('coral-multifield-item').length);
-                 $multifield.find('coral-multifield-item').each(function () {
-                   var $multifieldItem = $(this);
-                     console.log($multifieldItem.find('input[name]'));
-               });
-               });
-
-
-
-
-
-
-
-        var attributelist = $("input[name='./fname']").attr("data-attribute1").split(",");
-
-        console.log("attributelist" + attributelist.size);
-        var parentlabel = "./" + $("input[name='./fname']").attr("data-parentlabel");
-
-
-        var $formminmax = $(this).closest("form.foundation-form");
-
-        var field = $formminmax.find("[data-granite-coral-multifield-name='" + parentlabel + "']");
-        var totalLinkCount = field.children('coral-multifield-item').length;
-        var test = field.children('coral-Form-fieldwrapper').name;
-
-
-        var jsonObj = [];
-
-        for (var i = 0; i < totalLinkCount; i++) {
-            var item = {}
-            console.log("pp" + 'itemXX'.replace('XX', i));
-
-            for (var j = 0; j < attributelist.length; j++) {
-                var itemname = './bookdetailswithmap/item' + i + '/./' + attributelist[j];
-                item[attributelist[j]] = field.find($("[name='" + itemname + "']")[0]).val();
-            }
-            jsonObj.push(item);
-        }
-        var obj2 = { "parentlabel": jsonObj };
-        snippet.log(obj2.personalization.name);
-        $('.fnameclass').val(JSON.stringify(jsonObj));
+            setMultifield($multifield);
+        });
     });
+
+    $(document).on("foundation-contentloaded", function (e) {
+        var $form = $('form.cq-dialog.foundation-form');
+        var nodePath = $form.attr('action');
+        $.get(nodePath + '.json', function (data) {
+            populateMultifield(data);
+        });
+    });
+
+    function populateMultifield(data){
+        $('.multifield-save-is-json').each(function () {
+            var $multifield = $(this);
+            var multifieldname = $multifield.attr('data-granite-coral-multifield-name');
+            checkMultifieldInJsonResponse($multifield, multifieldname);
+        });
+    }
+
+    function checkMultifieldInJsonResponse($multifield, multifieldname){
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                if (multifieldname.replace('./', '') == key) {
+                    var multifieldJson = data[key];
+                    for (var index = 0; index < multifieldJson.length; index++) {
+                        var multifieldItemJson = JSON.parse(multifieldJson[index]);
+                        var multifieldItem = $multifield.find('coral-multifield-item')[index];
+                        var $multifieldItem = $(multifieldItem);
+                        console.log(multifieldItemJson);
+                        for (var n in multifieldItemJson) {
+                            var $input = $multifieldItem.find('[name="./' + n + '"]');
+                            console.log($input);
+                            $input.val(multifieldItemJson[n]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    function setMultifield($multifield) {
+        var multifieldname = $multifield.attr('data-granite-coral-multifield-name');
+        $multifield.find('coral-multifield-item').each(function () {
+            var $multifieldItem = $(this);
+            var inputs = $multifieldItem.find('input[name]');
+            var jsonObj = {};
+            inputs.each(function () {
+                var $input = $(this);
+                var key = $input.attr('name')?.replace('./', '');
+                if (!key.includes('@')) {
+                    jsonObj[key] = $input.val();
+                    $input.attr('disabled', true);
+                }
+            });
+            createHiddenInputField($multifield, multifieldname, JSON.stringify(jsonObj));
+        });
+        createHiddenInputField($multifield, multifieldname + '@TypeHint', 'String[]');
+    }
+
+    function createHiddenInputField(ele, name, value) {
+        var inputField = document.createElement('input');
+        inputField.setAttribute('type', 'hidden');
+        inputField.setAttribute('name', name);
+        inputField.setAttribute('value', value);
+        ele.append(newField);
+    }
+
+
 })(document, Granite.$, Granite.author);
+
+
