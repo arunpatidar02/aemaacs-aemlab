450a9ba3ca48b9954db0043142a5420070b95fa1
Arun Patidar <arunpatidar02>
2022-07-26 20:20:16 +0200

    link conversion stop

--- ui.apps/src/main/content/jcr_root/apps/aemlab/concept/clientlibs/author/clientlib-dialog-tinymce-editor-init/js/tinymce-init.js
+++ ui.apps/src/main/content/jcr_root/apps/aemlab/concept/clientlibs/author/clientlib-dialog-tinymce-editor-init/js/tinymce-init.js
@@ -6,49 +6,38 @@
         var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
         tinymce.init({
-          selector: '.tinymce-editor',
-          plugins: 'preview searchreplace autolink autosave save directionality visualblocks visualchars image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
-          mobile: {
-            plugins: 'preview searchreplace autolink autosave save directionality visualblocks visualchars image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbar emoticons'
-          },
-          menubar: 'file edit view insert format tools table help',
-          toolbar: 'undo redo | bold italic underline strikethrough link',
-          autosave_ask_before_unload: true,
-          autosave_interval: '30s',
-          autosave_prefix: '{path}{query}-{id}-',
-          autosave_restore_when_empty: false,
-          autosave_retention: '2m',
-          image_advtab: true,
-          link_list: [
-            { title: 'My page 1', value: 'https://www.tiny.cloud' },
-            { title: 'My page 2', value: 'http://www.moxiecode.com' }
-          ],
-          image_list: [
-            { title: 'My page 1', value: 'https://www.tiny.cloud' },
-            { title: 'My page 2', value: 'http://www.moxiecode.com' }
-          ],
-          image_class_list: [
-            { title: 'None', value: '' },
-            { title: 'Some class', value: 'class-name' }
-          ],
-          importcss_append: true,
-          templates: [
-            { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
-            { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
-            { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
-          ],
-          height: 600,
-          image_caption: true,
-          quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-          noneditable_noneditable_class: 'mceNonEditable',
-          toolbar_mode: 'sliding',
-          spellchecker_ignore_list: ['Ephox', 'Moxiecode'],
-          tinycomments_mode: 'embedded',
-          content_style: '.mymention{ color: gray; }',
-          contextmenu: 'link image imagetools table',
-          a11y_advanced_options: true,
-          skin: useDarkMode ? 'oxide-dark' : 'oxide',
-          content_css: useDarkMode ? 'dark' : 'default'
+            selector: '.tinymce-editor',
+            plugins: 'preview searchreplace autosave save directionality visualblocks visualchars image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+            mobile: {
+                plugins: 'preview searchreplace autosave save directionality visualblocks visualchars image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbar emoticons'
+            },
+            menubar: 'file edit view insert format tools table help',
+            toolbar: 'undo redo | bold italic underline strikethrough link',
+            autosave_ask_before_unload: true,
+            autosave_interval: '30s',
+            autosave_prefix: '{path}{query}-{id}-',
+            autosave_restore_when_empty: false,
+            autosave_retention: '2m',
+            image_advtab: true,
+            link_list: [
+                { title: 'AEMLAB', value: 'https://aemlab.blogspot.com/2019/06/index.html' },
+                { title: 'GitHub', value: 'https://github.com/arunpatidar02/aemaacs-aemlab' }
+            ],
+            image_class_list: [
+                { title: 'Landscape', value: 'aemlab-image__landscape' },
+                { title: 'Portrait', value: 'aemlab-image__portrait' }
+            ],
+            importcss_append: true,
+            height: 600,
+            image_caption: true,
+            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            noneditable_noneditable_class: 'mceNonEditable',
+            toolbar_mode: 'sliding',
+            convert_urls: false,
+            content_style: '.aemlab__somectyle{ color: gray; }',
+            contextmenu: 'link image imagetools table',
+            skin: useDarkMode ? 'oxide-dark' : 'oxide',
+            content_css: useDarkMode ? 'dark' : 'default'
         });
 
     });
