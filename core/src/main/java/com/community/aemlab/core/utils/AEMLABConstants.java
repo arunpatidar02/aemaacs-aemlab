3c014255bf00eea85a9ce755ab9c2d9c2a34d5a7
Arun Patidar <35201853+arunpatidar02@users.noreply.github.com>
2022-08-28 21:37:13 +0200

    context aware configuration - header & footer (#23)

--- core/src/main/java/com/community/aemlab/core/utils/AEMLABConstants.java
+++ core/src/main/java/com/community/aemlab/core/utils/AEMLABConstants.java
@@ -5,13 +5,27 @@ package com.community.aemlab.core.utils;
  *
  */
 public class AEMLABConstants {
-	
-	AEMLABConstants(){
-		
+
+	AEMLABConstants() {
+
 	}
-	
+
 	public static final String CONTENTTYPE_TXT_HTML = "text/html";
 	public static final String AEMLAB_SUBSERVICE_READ = "aemlab-service-read";
 	public static final String AEMLAB_SUBSERVICE_WRITE = "aemlab-service-write";
+	
+	public static final String COMMA = ",";
+
+	public static final String HYPHEN = "-";
+
+	public static final String FORWARD_SLASH = "/";
+
+	public static final String SEARCH_SEQ = "../";
+	
+	public static final String CONTENT_AEMLAB_ROOT = "/content/aemlab";
+	
+	public static final String CONTENT_XF_AEMLAB_ROOT = "/content/experience-fragments/aemlab";
+	
+	
 
 }
