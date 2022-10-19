27d0d854046b0c46d61a0ce22d4a1d50201f88fc
Arun Patidar <arunpatidar02>
2022-10-19 13:33:04 +0200

    sonarfix - import update

--- core/src/main/java/com/community/aemlab/core/filters/sample/WebcomponentDynamicFilter.java
+++ core/src/main/java/com/community/aemlab/core/filters/sample/WebcomponentDynamicFilter.java
@@ -20,7 +20,7 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.jcr.resource.JcrResourceConstants;
+import org.apache.sling.jcr.resource.api.JcrResourceConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
