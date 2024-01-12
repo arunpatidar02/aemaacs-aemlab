a072607fc7ac5016a1f5c95974770b17de10719d
Arun Patidar <arunpatidar02>
2024-01-12 12:01:25 +0100

    js fix

--- ui.apps/src/main/content/jcr_root/apps/aemlab/oneweb/concept/clientlibs/author/clientlib-dialog-tag-picker/js/tag-picker-ns-validation.js
+++ ui.apps/src/main/content/jcr_root/apps/aemlab/oneweb/concept/clientlibs/author/clientlib-dialog-tag-picker/js/tag-picker-ns-validation.js
@@ -3,31 +3,28 @@
     SELECTOR: '[data-foundation-validation=tag-namespace-validator]',
     ATTR_NAME: 'name',
     ERROR_MESSAGE: 'Valid Namespaces are : {0}',
-    ATTR_NAMESAPCE: 'validnamespaces',
+    ATTR_NAMESPACE: 'validnamespaces',
     COLON: ':',
     COMMA: ',',
   };
 
-  $(window).adaptTo('foundation-registry').register('foundation.validation.validator', {
+  const validateTagNamespace = (el) => {
+    const validNamespaces = el.dataset[CONST.ATTR_NAMESPACE]?.split(CONST.COMMA) || [];
+    const eleNameProp = el.getAttribute(CONST.ATTR_NAME);
+    const tagInputs = el.parentElement.querySelectorAll(`input[name="${eleNameProp}"]`);
 
+    return Array.from(tagInputs).some((input) => {
+      const { value } = input;
+      return !validNamespaces.includes(value?.split(CONST.COLON)[0]);
+    });
+  };
+
+  $(window).adaptTo('foundation-registry').register('foundation.validation.validator', {
     selector: CONST.SELECTOR,
     validate(el) {
-      const validNamesapces = el.dataset.validnamespaces?.split(CONST.COMMA);
-      const eleNameProp = el.getAttribute(CONST.ATTR_NAME);
-      let inValid = false;
-
-      const tagInputs = el.parentElement.querySelectorAll(`input[name="${eleNameProp}"]`);
-      tagInputs.forEach((input) => {
-        const { value } = input;
-        if (!validNamesapces.includes(value?.split(CONST.COLON)[0])) {
-          inValid = true;
-        }
-      });
-
-      if (inValid) {
-        return Granite.I18n.get(CONST.ERROR_MESSAGE.replace('{0}', validNamesapces));
-      }
-      return '';
+      return validateTagNamespace(el)
+        ? Granite.I18n.get(CONST.ERROR_MESSAGE.replace('{0}', el.dataset[CONST.ATTR_NAMESPACE]))
+        : '';
     },
   });
 }(window, Granite, Granite.$));
