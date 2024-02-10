e31fc62bfd147e56ecaca740c6e2739c66c75f75
Arun Patidar <35201853+arunpatidar02@users.noreply.github.com>
2024-02-10 11:51:50 +0100

    change teaser dialog when inside tabs (#33)

---
+++ ui.apps/src/main/content/jcr_root/apps/aemlab/oneweb/concept/clientlibs/author/clientlib-dialog-teaser-inside-tabs/js/teaser-inside-tabs.js
@@ -0,0 +1,48 @@
+(function (document, $) {
+    "use strict";
+  
+    const PARENT_TYPE = 'wknd/components/tabs';
+    const TYPE_PROPERTY = 'sling:resourceType';
+    const FORM_SELECTOR = '.cq-Dialog form.cq-dialog';
+  
+    $(document).on("foundation-contentloaded", function () {
+  
+      const dialogForm = document.querySelector(FORM_SELECTOR);
+      const contentPath = dialogForm.action;
+      const parentPath = getContainerPath(contentPath);
+  
+      fetchParentResourceType(parentPath)
+        .then(type => {
+          if (type === PARENT_TYPE) {
+            console.log('Tabs component');
+            // write logic to hide few fields
+          }
+        })
+        .catch(error => {
+          console.error('Fetch error:', error);
+        });
+    });
+  
+    function getContainerPath(contentPath) {
+      const url = new URL(contentPath);
+      const pathSegments = url.pathname.split('/');
+      const newPathSegments = pathSegments.slice(0, -2);
+      return newPathSegments.join('/');
+    }
+  
+    function fetchParentResourceType(parentPath) {
+      return fetch(`${parentPath}.json`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => data?.[TYPE_PROPERTY])
+        .catch(error => {
+          throw new Error('Fetch error:', error);
+        });
+    }
+  
+  })(document, Granite.$);
+  
